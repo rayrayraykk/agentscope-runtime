c7b51e3e9387b34168459df2d01d5c2b1314facb
raykkk <weirui.kwr@alibaba-inc.com>
2025-09-05 16:06:35 +0800

    update

--- cookbook/build.sh
+++ cookbook/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Multiple versions to build
-VERSIONS=("latest" "v0.1.0" "v0.1.1" "v0.1.2")
+VERSIONS=("latest" "v0.1.1" "v0.1.2")
 OUTPUT_DIR="_build"
 
 # ANSI color codes for better display
