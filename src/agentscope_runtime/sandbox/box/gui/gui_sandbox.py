dc3410ec0076c32a9dc8d2653d9406d8f540b614
raykkk <weirui.kwr@alibaba-inc.com>
2025-10-09 15:07:13 +0800

    add gui sandbox

---
+++ src/agentscope_runtime/sandbox/box/gui/gui_sandbox.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from typing import Optional
+
+from ...utils import build_image_uri
+from ...registry import SandboxRegistry
+from ...enums import SandboxType
+from ...box.sandbox import Sandbox
+
+
+@SandboxRegistry.register(
+    build_image_uri("runtime-sandbox-gui"),
+    sandbox_type=SandboxType.GUI,
+    security_level="high",
+    timeout=30,
+    description="GUI Sandbox",
+)
+class GuiSandbox(Sandbox):
+    def __init__(
+        self,
+        sandbox_id: Optional[str] = None,
+        timeout: int = 3000,
+        base_url: Optional[str] = None,
+        bearer_token: Optional[str] = None,
+    ):
+        super().__init__(
+            sandbox_id,
+            timeout,
+            base_url,
+            bearer_token,
+            SandboxType.BASE,
+        )
+
+    def run_ipython_cell(self, code: str):
+        return self.call_tool("run_ipython_cell", {"code": code})
+
+    def run_shell_command(self, command: str):
+        return self.call_tool("run_shell_command", {"command": command})
