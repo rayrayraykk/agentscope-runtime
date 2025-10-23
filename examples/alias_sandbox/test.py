35b091e45cca856baa3ab057ee33e68333374976
raykkk <weirui.kwr@alibaba-inc.com>
2025-10-23 16:39:48 +0800

    update

--- examples/alias_sandbox/test.py
+++ examples/alias_sandbox/test.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
+# pylint:disable=no-name-in-module
 from alias_sandbox import AliasSandbox
 
 with AliasSandbox() as sandbox:
+    print(sandbox.desktop_url)
+    input("Press Enter to continue...")
     print(sandbox.sandbox_id)
     print(sandbox.run_ipython_cell("import time\ntime.sleep(1)"))
+    print(
+        sandbox.call_tool(
+            "browser_navigate",
+            arguments={"url": "https://www.google.com/"},
+        ),
+    )
     input("Press Enter to continue...")
