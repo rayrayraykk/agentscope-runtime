a14e736167a5157fc0fce9bf27efbcdf91442a77
raykkk <weirui.kwr@alibaba-inc.com>
2025-12-09 20:43:47 +0800

    support real stream tool results

--- src/agentscope_runtime/adapters/agentscope/stream.py
+++ src/agentscope_runtime/adapters/agentscope/stream.py
@@ -48,7 +48,7 @@ async def adapt_agentscope_message_stream(
     should_start_message = True
     should_start_reasoning_message = True
     tool_use_messages_dict = {}
-    # tool_result_messages_dict = {}
+    tool_result_messages_dict = {}
     index = None
 
     # Run agent
@@ -345,35 +345,74 @@ async def adapt_agentscope_message_stream(
                                 msg_type = MessageType.MCP_TOOL_CALL_OUTPUT
                                 fc_cls = McpCallOutput
 
+                        plugin_output_message = tool_result_messages_dict.get(
+                            call_id,
+                        )
+
+                        if plugin_output_message is None:
+                            # Create a new message
+                            plugin_output_message = Message(
+                                type=msg_type,
+                                role="tool",
+                            )
+                            data_delta_content = DataContent(
+                                index=0,
+                                data=fc_cls(
+                                    call_id=element.get("id"),
+                                    name=element.get("name"),
+                                    output="",
+                                ).model_dump(),
+                                delta=False,
+                            )
+                            plugin_output_message = _update_obj_attrs(
+                                plugin_output_message,
+                                metadata=metadata,
+                                usage=usage,
+                            )
+                            yield plugin_output_message.in_progress()
+                            yield data_delta_content.in_progress()
+                            tool_result_messages_dict[
+                                call_id
+                            ] = plugin_output_message
+
+                        # Update output
                         json_str = json.dumps(
                             element.get("output"),
                             ensure_ascii=False,
                         )
                         data_delta_content = DataContent(
-                            index=index,
+                            index=None if last else 0,
                             data=fc_cls(
                                 call_id=element.get("id"),
                                 name=element.get("name"),
                                 output=json_str,
                             ).model_dump(),
+                            delta=False,
                         )
-                        plugin_output_message = Message(
-                            type=msg_type,
-                            role="tool",
-                            content=[data_delta_content],
-                        )
+
                         plugin_output_message = _update_obj_attrs(
                             plugin_output_message,
                             metadata=metadata,
                             usage=usage,
                         )
-                        yield plugin_output_message.completed()
-                        message = Message(
-                            type=MessageType.MESSAGE,
-                            role="assistant",
-                        )
-                        should_start_message = True
-                        index = None
+
+                        if last:
+                            plugin_output_message.add_content(
+                                new_content=data_delta_content,
+                            )
+                            yield data_delta_content.completed()
+                            yield plugin_output_message.completed()
+
+                            message = Message(
+                                type=MessageType.MESSAGE,
+                                role="assistant",
+                            )
+                            should_start_message = True
+                            index = None
+
+                        else:
+                            yield data_delta_content.in_progress()
+
                     else:
                         # TODO: handle image/audio/video block
                         if should_start_message:
